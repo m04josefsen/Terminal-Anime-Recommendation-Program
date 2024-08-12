856f164fb5d57c7249e38a76b7592a3016f65278
Marco Josefsen <majos6959@oslomet.no>
2024-08-12 16:00:50 +0200

    Added Jackson Dependecy for ObjectMapper and JsonNode

--- src/main/java/UserInteraction.java
+++
@@ -1,6 +0,0 @@
-public class UserInteraction {
-    public static void show() {
-        String print = "Copy your Anime List from anilist.co and paste it under";
-        System.out.println(print);
-    }
-}
