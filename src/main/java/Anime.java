c221832aa5f69f4b315fb1d65de998f85f55d037
Marco Josefsen <majos6959@oslomet.no>
2024-08-13 22:55:52 +0200

    Refactored Anime -> InputAnime, and SearchedAnime -> Anime

--- src/main/java/Anime.java
+++ src/main/java/Anime.java
@@ -1,10 +1,28 @@
-public class Anime {
-    private String title;
-    private double rating;
+import java.util.List;
 
-    public Anime(String title, double rating) {
+public class Anime {
+    private int malId;
+    private String title;
+    private double score;
+    private String imageUrl;
+    private String description;
+    private List<String> genres;
+
+    public Anime(int malId, String title, double score, String imageUrl, String description, List<String> genres) {
+        this.malId = malId;
         this.title = title;
-        this.rating = rating;
+        this.score = score;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.genres = genres;
+    }
+
+    public int getMalId() {
+        return malId;
+    }
+
+    public void setMalId(int malId) {
+        this.malId = malId;
     }
 
     public String getTitle() {
@@ -15,17 +33,49 @@ public class Anime {
         this.title = title;
     }
 
-    public double getRating() {
-        return rating;
+    public double getScore() {
+        return score;
     }
 
-    public void setRating(double rating) {
-        this.rating = rating;
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
     }
 
     public String toString() {
-        String print = "Anime title: " + title;
-        print += "\nRating: " + rating;
+        String print = "MyAnimeList ID: " + malId;
+        print += "\nTitle: " + title;
+        print += "\nScore: " + score;
+        print += "\nImageUrl: " + imageUrl;
+        print += "\nDescription: " + description;
+        print += "\nGenres: ";
+        for(String genre : genres) {
+            print += "\n- " + genre;
+        }
+
         return print;
     }
 }
+
---
+++ src/main/java/InputAnime.java
@@ -0,0 +1,31 @@
+public class InputAnime {
+    private String title;
+    private double rating;
+
+    public InputAnime(String title, double rating) {
+        this.title = title;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String toString() {
+        String print = "Anime title: " + title;
+        print += "\nRating: " + rating;
+        return print;
+    }
+}
